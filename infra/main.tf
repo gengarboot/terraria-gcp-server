cead86d47fc19f4ab3c95590d16a0d1f921f60ff
gengarboot <gengarboot@fastmail.com>
2025-09-25 14:38:56 +0200

    currently machine has no internet access

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,27 @@
+locals {
+  startup_script = file("../scripts/startup.sh")
+}
 resource "google_compute_instance" "server" {
     name = "terraria-server"
-    machine_type = "e2-micro"
+    machine_type = "e2-small"
 
     # Zone marked as optional in documentation but seems required
     zone = "europe-north2-a" 
     boot_disk {
       initialize_params {
-        image = "ubuntu-os-cloud/ubuntu-minimal-2504-plucky-amd64-v20250923"
+        image = "ubuntu-os-cloud/ubuntu-2404-noble-amd64-v20250924"
       }
     }
+
+    # Network tags?
+    tags = ["https-server", "http-server"]
+
     network_interface {
         # What does this mean?
-        network = "default"
+        network = "default" 
+    }
+
+    metadata = {
+        startup-script = local.startup_script
     }
 }
---
+++ scripts/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "hello" > hello.txt
+mkdir terraria-server
+cd  terraria-server
+wget https://terraria.org/api/download/pc-dedicated-server/terraria-server-1449.zip
