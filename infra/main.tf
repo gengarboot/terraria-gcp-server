aed242b2c3f6d6ce302f570aab2c76f5f3040e7e
gengarboot <gengarboot@fastmail.com>
2025-09-25 13:11:30 +0200

    adding zone

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,9 @@
 resource "google_compute_instance" "server" {
     name = "terraria-server"
     machine_type = "e2-micro"
-    zone = "Any"
+    
+    # Zone marked as optional in documentation but seems required
+    zone = "europe-north2-a" 
     boot_disk {
       initialize_params {
         image = "ubuntu-os-cloud/ubuntu-minimal-2504-plucky-amd64-v20250923"
